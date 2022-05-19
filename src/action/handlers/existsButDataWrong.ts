00ceb8673003b13991843ef66df2acdeff9e2c6d
Kyle Hensel <k.y.l.e@outlook.co.nz>
2022-05-19 12:06:32 +1200

    attempt to respect changes to addr:housenumber
    If a user change the house number, we now change the linz ref to match the swapped housenumber in simple cases, instead of undoing the user's edit

--- src/action/handlers/existsButDataWrong.ts
+++ src/action/handlers/existsButDataWrong.ts
@@ -11,6 +11,9 @@ import {
 } from '../../types';
 import { createDiamond, outFolder, toLink, fieldsToModify } from '../util';
 
+const toKey = (right: string, wrong: string, osmData: OsmAddr) =>
+  `${[right, wrong].sort().join('__')}__${osmData.street}`;
+
 export async function existsButDataWrong(
   arr: StatusReport[Status.EXISTS_BUT_WRONG_DATA],
 ): Promise<HandlerReturn> {
@@ -19,21 +22,89 @@ export async function existsButDataWrong(
     ac[suburb] ||= [];
     ac[suburb].push([linzId, osmAddr, issues]);
     return ac;
-  }, {} as Record<string, [string, OsmAddr, Issue[]][]>);
+  }, {} as Record<string, [linzId: string, osmAddr: OsmAddr, issues: Issue[], swap?: boolean][]>);
 
   let report = '';
-  const index: HandlerReturn = {};
+  const out: HandlerReturn = {};
 
   for (const suburb in bySuburb) {
     report += `\n${suburb}\n`;
 
     const features: GeoJsonFeature[] = [];
 
-    for (const [linzId, osmData, issues] of bySuburb[suburb]) {
-      // const [key, wrong, right] = issue.split('|');
-      report += `${linzId}\t\t${toLink(osmData.osmId)}\t\t${issues.join(
-        '\tand\t',
-      )}\n`;
+    // search for pairs where the housenumber was simply swapped
+    // this is a common occurance, e.g. someone swaps 14A and 14B
+    // but doesn't change the linzref
+    const maybeSwappablePairs: Record<
+      string,
+      { index: number; linzId: string; osmData: OsmAddr }[]
+    > = {};
+    for (let i = 0; i < bySuburb[suburb].length; i += 1) {
+      const [linzId, osmData, issues] = bySuburb[suburb][i];
+
+      const [type, right, wrong] = issues[0].split('|');
+      if (issues.length === 1 && type === 'housenumber') {
+        const key = toKey(right, wrong, osmData);
+        maybeSwappablePairs[key] ||= [];
+        maybeSwappablePairs[key].push({ index: i, linzId, osmData });
+      }
+    }
+
+    for (const key in maybeSwappablePairs) {
+      const swappable = maybeSwappablePairs[key];
+      if (swappable.length === 2) {
+        const [a, b] = swappable;
+
+        features.push({
+          type: 'Feature',
+          id: a.osmData.osmId,
+          geometry: {
+            type: 'Polygon',
+            coordinates: createDiamond(a.osmData),
+          },
+          properties: {
+            __action: 'edit',
+            // instead of changing the housenumber, we will swap the linz ref
+            'ref:linz:address_id': b.linzId,
+
+            // maybe remove the check_date tag if it's out-of-date
+            ...(a.osmData.checked === CheckDate.YesExpired && {
+              check_date: '🗑️',
+            }),
+          },
+        });
+        features.push({
+          type: 'Feature',
+          id: b.osmData.osmId,
+          geometry: {
+            type: 'Polygon',
+            coordinates: createDiamond(b.osmData),
+          },
+          properties: {
+            __action: 'edit',
+            // instead of changing the housenumber, we will swap the linz ref
+            'ref:linz:address_id': a.linzId,
+
+            // maybe remove the check_date tag if it's out-of-date
+            ...(b.osmData.checked === CheckDate.YesExpired && {
+              check_date: '🗑️',
+            }),
+          },
+        });
+
+        // tell the next loop that these ones have already been handled
+        bySuburb[suburb][a.index][3] = true;
+        bySuburb[suburb][b.index][3] = true;
+      }
+    }
+
+    for (const [linzId, osmData, issues, swap] of bySuburb[suburb]) {
+      report += `${linzId}\t\t${toLink(osmData.osmId)}\t\t${
+        swap ? '(swap) ' : ''
+      }${issues.join('\tand\t')}\n`;
+
+      // we'll deal with this one later
+      if (swap) continue; // eslint-disable-line no-continue
 
       features.push({
         type: 'Feature',
@@ -51,10 +122,10 @@ export async function existsButDataWrong(
       });
     }
 
-    index[`Address Update - ${suburb}`] = features;
+    out[`Address Update - ${suburb}`] = features;
   }
 
   await fs.writeFile(join(outFolder, 'data-wrong.txt'), report);
 
-  return index;
+  return out;
 }
