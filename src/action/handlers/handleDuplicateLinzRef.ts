88c7fa0d908f8d5f4ccdfa0342a3a54f15ef0ce8
Kyle Hensel <k.y.l.e@outlook.co.nz>
2022-10-27 17:11:45 +1300

    autofix more situations involving duplicate ref:linz:address_id tags

--- src/action/handlers/handleDuplicateLinzRef.ts
+++ src/action/handlers/handleDuplicateLinzRef.ts
@@ -16,41 +16,15 @@ const toKey = (addr: LinzAddr | OsmAddr) =>
 export async function handleDuplicateLinzRef(
   arr: StatusReport[Status.MULTIPLE_EXIST],
 ): Promise<HandlerReturn> {
-  const bySuburb = arr.reduce((ac, [linzId, [linzAddr, osmAddrList]]) => {
-    const suburb = linzAddr.suburb[1];
-    // eslint-disable-next-line no-param-reassign -- mutation is cheap
-    ac[suburb] ||= [];
-    ac[suburb].push([linzId, linzAddr, osmAddrList]);
-    return ac;
-  }, {} as Record<string, [linzId: string, linzAddr: LinzAddr, osmAddrList: OsmAddr[]][]>);
-
-  let report = '';
-  for (const suburb in bySuburb) {
-    report += `\n${suburb}\n`;
-    for (const [linzId, , osmIdList] of bySuburb[suburb]) {
-      report += `${linzId}\t\texists on ${osmIdList
-        .map((addr) => toLink(addr.osmId))
-        .join(' and ')}\n`;
-    }
-  }
-
-  await fs.writeFile(join(outFolder, 'duplicate-linz-ref.txt'), report);
-
+  const autofixable: Record<string, '✅' | '⚠️'> = {};
   const features: GeoJsonFeature[] = [];
 
-  for (const [, [linzAddr, osmAddrList]] of arr) {
+  for (const [linzId, [linzAddr, osmAddrList]] of arr) {
     const simpleNodes = osmAddrList.filter(
       (x) => x.osmId[0] === 'n' && !x.isNonTrivial,
     );
-    const buildings = osmAddrList.filter((x) => x.osmId[0] !== 'n');
 
-    // we can autofix this if there are only 2 addresses, where one is a simple node and one is a building
-    // Then it's pretty much the same as diagnostic 15 (REPLACED_BY_BUILDING).
-    // More complex situations need to be manually fixed.
-    const isSimpleCase =
-      osmAddrList.length === 2 &&
-      simpleNodes.length === 1 &&
-      buildings.length === 1;
+    // we can autofix this if some of the duplicates are simple nodes
 
     // another case we can autofix is if one node has info that doesn't match the linz ref
     // e.g. No.12 and No.12A both have the linz ref for No.12
@@ -72,22 +46,60 @@ export async function handleDuplicateLinzRef(
           },
           properties: { __action: 'edit', 'ref:linz:address_id': '🗑️' },
         });
+        autofixable[linzId] = '✅';
       }
-    } else if (isSimpleCase) {
-      // delete the simpleNode. Don't need to edit the building
-      const toDelete = simpleNodes[0];
+    } else if (simpleNodes.length) {
+      // Either (a) all nodes are simple. Arbitrarily pick 1 to keep and delete the rest.
+      // Or     ( b) notall of the addresses are simple, so delete only the simple ones.
+      const toDelete =
+        simpleNodes.length === osmAddrList.length
+          ? simpleNodes.slice(1)
+          : simpleNodes;
 
-      features.push({
-        type: 'Feature',
-        id: toDelete.osmId,
-        geometry: {
-          type: 'Polygon',
-          coordinates: createSquare(toDelete),
-        },
-        properties: { __action: 'delete' },
-      });
+      const notFullyFixed = osmAddrList.length - simpleNodes.length > 1;
+
+      // delete the simple nodes
+      for (const addr of toDelete) {
+        features.push({
+          type: 'Feature',
+          id: addr.osmId,
+          geometry: {
+            type: 'Polygon',
+            coordinates: createSquare(addr),
+          },
+          properties: { __action: 'delete' },
+        });
+      }
+      autofixable[linzId] = notFullyFixed ? '⚠️' : '✅';
     }
   }
+
+  const bySuburb = arr.reduce((ac, [linzId, [linzAddr, osmAddrList]]) => {
+    const suburb = linzAddr.suburb[1];
+    // eslint-disable-next-line no-param-reassign -- mutation is cheap
+    ac[suburb] ||= [];
+    ac[suburb].push([linzId, linzAddr, osmAddrList]);
+    return ac;
+  }, {} as Record<string, [linzId: string, linzAddr: LinzAddr, osmAddrList: OsmAddr[]][]>);
+
+  let report = [
+    '✅ = issue can be autofixed',
+    '⚠️ = issue can be partially autofixed',
+    '❌ = issue cannot be autofixed',
+    '',
+  ].join('\n');
+  for (const suburb in bySuburb) {
+    report += `\n${suburb}\n`;
+    for (const [linzId, , osmIdList] of bySuburb[suburb]) {
+      report += `${linzId}\t${
+        autofixable[linzId] || '❌'
+      }\texists on ${osmIdList
+        .map((addr) => toLink(addr.osmId))
+        .join(' and ')}\n`;
+    }
+  }
+
+  await fs.writeFile(join(outFolder, 'duplicate-linz-ref.txt'), report);
 
   if (!features.length) return {};
 
