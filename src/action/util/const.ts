bd0f20daa36ee612978412b1e87d8a12483793b2
Kyle Hensel <k.y.l.e@outlook.co.nz>
2024-07-06 18:03:34 +1000

    make `MAX_ITEMS_PER_DATASET` configurable

--- src/action/util/const.ts
+++ src/action/util/const.ts
@@ -19,4 +19,6 @@ export const MAP = <const>{
 export const toLink = (osmId: string): string =>
   `https://osm.org/${MAP[osmId[0] as keyof typeof MAP]}/${osmId.slice(1)}`;
 
-export const MAX_ITEMS_PER_DATASET = 110;
+export const MAX_ITEMS_PER_DATASET = +(
+  process.env.MAX_ITEMS_PER_DATASET || 110
+);
