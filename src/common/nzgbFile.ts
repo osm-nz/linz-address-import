ab10d8d328141604ee84cb598a5de89267fe4d58
Kyle Hensel <k.y.l.e@outlook.co.nz>
2024-07-18 21:48:40 +1000

    add override

--- src/common/nzgbFile.ts
+++ src/common/nzgbFile.ts
@@ -20,6 +20,7 @@ export const nzgbNamesTable: Record<string, string> = {
   'Lake Okareka': 'Lake Okareka', // https://gazetteer.linz.govt.nz/place/7385
   Ngahape: 'Ngāhape', // https://gazetteer.linz.govt.nz/place/32282
   Peketa: 'Peketā', // https://gazetteer.linz.govt.nz/place/57309
+  Orakei: 'Ōrākei', // https://gazetteer.linz.govt.nz/place/33996
 
   Opotiki: 'Ōpōtiki', // // only included here for unit tests. It's correct in the LINZ dataset
 };
