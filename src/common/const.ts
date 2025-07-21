5688240d108d5c5f86edee776e3bad83a49eee7a
Kyle Hensel <k.y.l.e@outlook.co.nz>
2025-07-21 20:25:58 +1000

    the AIM layer id seems to have changed

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,7 +1,7 @@
 export const LINZ_LAYER = 105689;
 export const LINZ_LAYER_NAME_SUBSTR = 'nz-addresses';
 
-export const AIM_LAYER = 3324;
+export const AIM_LAYER = 53324;
 export const AIM_LAYER_NAME_SUBSTR = 'aims-address';
 
 export const RURAL_URBAN_LAYER = 111198;
